53d53aec59ac40619a33f3175c97f78dbe037287
Richard Addo <ramankwah03@gmail.com>
2024-09-02 19:21:25 +0200

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,28 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "selector",
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      backgroundImage: {},
+      colors: {
+        primary: {
+          violet: "#635FC7",
+          "light-violet": "#A8A4FF",
+          dark: "#000112",
+          "semi-dark": "#20212C",
+        },
+        secondary: {
+          "dark-gray": "#2B2C37",
+          "light-gray": "#3E3F4E",
+          gray: "#828FA3",
+          "light-blue": "#E4EBFA",
+        },
+        tetiary: {
+          "white-space": "#F4F7FD",
+          red: "#EA5555",
+          "light-red": "#FF9898",
+        },
       },
     },
   },
